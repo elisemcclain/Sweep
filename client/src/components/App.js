560309fd37df16f81e59414bfa9470a9e99d777d
elisemcclain <celisemcclain@gmail.com>
2023-09-07 14:49:42 -0500

    test

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-// import GoblinContainer from "./Goblins/GoblinContainer";
-// import GoblinDetails from "./Goblins/GoblinDetails";
-// import Home from "./Home";
-// import Login from "./Login";
-// import UserPage from "./UserPage";
-// import NavBar from "./NavBar";
-// import Game from "./Game";
-
 function App() {
-  return <h1>Hello</h1>;
-}
+  console.log("hello");
 
+  return (
+    <div>
+      <h1>Hello</h1>;
+    </div>
+  );
+}
 export default App;
