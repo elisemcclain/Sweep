ec1a08888489093feda839710e8d64dc92a250a8
elisemcclain <celisemcclain@gmail.com>
2023-09-07 14:27:41 -0500

    files

--- client/my-react-app/src/Hamburger.js
+++
@@ -1,9 +0,0 @@
-export default function Hamburger({ onClick }) {
-  return (
-    <div className="hamburger-icon" onClick={onClick}>
-      <div className="bar" />
-      <div className="bar" />
-      <div className="bar" />
-    </div>
-  );
-}
---
+++ client/src/components/App.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+// import GoblinContainer from "./Goblins/GoblinContainer";
+// import GoblinDetails from "./Goblins/GoblinDetails";
+// import Home from "./Home";
+// import Login from "./Login";
+// import UserPage from "./UserPage";
+// import NavBar from "./NavBar";
+// import Game from "./Game";
+
+function App() {
+  return <h1>Hello</h1>;
+}
+
+export default App;
