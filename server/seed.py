0f94d62484b2d4b6e2edc917cd3a7071980c99a6
elisemcclain <celisemcclain@gmail.com>
2023-10-09 18:30:13 -0500

    new seed

--- server/seed.py
+++ server/seed.py
@@ -9,14 +9,14 @@ from config import db, app
 
 def create_users():
     users = [
-        User(first_name='Susan', last_name='Gladiator', email='susan@g.com', is_active=None, password_hash='ssd1ed31qe11'),
-        User(first_name='Zendaya', last_name='Holland', email='byre@dedbdfdysree.ffcom', is_active=None, password_hash='d1qe13dd12'),
-        User(first_name='Margaret', last_name='Bulle', email='maggie@hi.co,', is_active=None, password_hash='kkkkkk'),
-        User(first_name='Barbara', last_name='Shank', email='de@de.cofm', is_active=None, password_hash='dddddd'),
-        User(first_name='Benjamin', last_name='Button', email='benny@button.ffcom', is_active=None, password_hash='d1qe13dsd12'),
-        User(first_name='Glen', last_name='Dale', email='glen@dale.co,', is_active=None, password_hash='kkkkakk'),
-        User(first_name='Billy Bob', last_name='Buccee', email='billybob@buccee.cofm', is_active=None, password_hash='ssd1ead31qe11'),
-        User(first_name='Azelia', last_name='Thorn', email='azelia@thorn.ffcom', is_active=None, password_hash='vdasdt345'),
+        User(first_name='Susan', last_name='Gladiator', email='1susan@susan.com', is_active=None, password_hash='ssd1ed31qe11'),
+        User(first_name='Zendaya', last_name='Holland', email='1zendaya@dedbdfdysree.com', is_active=None, password_hash='d1qe13dd12'),
+        User(first_name='Margaret', last_name='Bulle', email='1maggie@himaggie.com,', is_active=None, password_hash='kkkkkk'),
+        User(first_name='Barbara', last_name='Shank', email='1de@deshank.com', is_active=None, password_hash='dddddd'),
+        User(first_name='Benjamin', last_name='Button', email='1benjamin@button.com', is_active=None, password_hash='d1qe13dsd12'),
+        User(first_name='Glen', last_name='Dale', email='1glen@glendale.com,', is_active=None, password_hash='kkkkakk'),
+        User(first_name='Billy Bob', last_name='Buccee', email='1billybob@buccees.com', is_active=None, password_hash='ssd1ead31qe11'),
+        User(first_name='Azelia', last_name='Thorn', email='1azelias@thorn.com', is_active=None, password_hash='vdasdt345'),
     ]
     db.session.add_all(users)
     db.session.commit()
@@ -39,16 +39,9 @@ def create_crime():
     with app.app_context():
         session = db.session
 
-        location1 = session.get(Location, 1)
-        location2 = session.get(Location, 2)
-        location3 = session.get(Location, 3)
-        location4 = session.get(Location, 4)
-        location5 = session.get(Location, 5)
-        location6 = session.get(Location, 6)
-        location7 = session.get(Location, 7)
-        location8 = session.get(Location, 8)
+        locations = session.query(Location).all()
 
-        if location1 and location2:
+        if locations:
             crime1 = Crime(name='Stolen Property', desc='Woman\'s favorite cat is stolen', date=datetime(2021, 12, 17))
             crime2 = Crime(name='Lurker', desc='Strange figure lurking in backyard', date=datetime(2021, 10, 16))
             crime3 = Crime(name='Murder', desc='Woman found shot behind dumpsters', date=datetime(2021, 10, 16))
@@ -58,14 +51,15 @@ def create_crime():
             crime7 = Crime(name='Tax Fraud', desc='Contact IRS for more information', date=datetime(2021, 10, 16))
             crime8 = Crime(name='Noise Complaint', desc='Yodeling heard at 3am', date=datetime(2021, 10, 16))
 
-            crime1.locations.append(location1)
-            crime2.locations.append(location2)
-            crime2.locations.append(location3)
-            crime2.locations.append(location4)
-            crime2.locations.append(location5)
-            crime2.locations.append(location6)
-            crime2.locations.append(location7)
-            crime2.locations.append(location8)
+            for location in locations:
+                crime1.locations.append(location)
+                crime2.locations.append(location)
+                crime2.locations.append(location)
+                crime2.locations.append(location)
+                crime2.locations.append(location)
+                crime2.locations.append(location)
+                crime2.locations.append(location)
+                crime2.locations.append(location)
 
             session.add_all([crime1, crime2, crime3, crime4, crime5, crime6, crime7, crime8])
             session.commit()
